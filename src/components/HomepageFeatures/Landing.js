c7c84579927b7f72e016856742cb538cc437ebfa
Medhavi Basera <1905781@kiit.ac.in>
2022-04-23 01:11:16 +0530

    :lipstick: Landing page updated

---
+++ src/components/HomepageFeatures/Landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Landing.css";
+export default function hiii() {
+  return (
+    <div className="container about">
+      <div className="cols">
+        <div className="rows">
+          <h1>Next.js Handbook 📚</h1>
+        </div>
+        <div className="rows">
+          <p>
+            This website is my handbook 📖 ❤️ , I will be documenting everything
+            as I learn Next.js 🎯.
+          </p>
+          <button className="cta-btn">Notes </button>
+        </div>
+      </div>
+    </div>
+  );
+}
